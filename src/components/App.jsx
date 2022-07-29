b4f15b43b28c6da6ff88df52fbe99f48e0b2eb19
Yevhen <krenichniy1995@gmail.com>
2022-07-29 09:30:29 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import Form from './Form';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+        return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+          >
+            <Form/>
+        React homework template
+      </div>
+    );
+   }
+
+};
+
+export default App;
