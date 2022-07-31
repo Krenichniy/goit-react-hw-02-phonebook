112561eae9132be840255d799e5633434c881bb3
Yevhen <krenichniy1995@gmail.com>
2022-07-31 23:04:46 +0300

    added files for styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components'
+import {Container, Header, LabelContainer, UserInput } from '../Form/Form.styled'
 
 class Filter extends Component {
     static propTypes = {
@@ -12,7 +12,7 @@ class Filter extends Component {
         return (
             <>
                 <Container>
-                <Heder>Contacts</Heder>
+                <Header>Contacts</Header>
                 <LabelContainer>Find contacts by Name
             
                 <UserInput
@@ -28,43 +28,5 @@ class Filter extends Component {
     }
 }
 
-const Container = styled.div`
-            max-width:450px;
-            margin-left:auto;
-            margin-right:auto;
-            margin-bottom:30px;
-            padding:15px;
-            background: #555555;
-            border-radius: 4px;
-            box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
-                0px 2px 1px rgba(0, 0, 0, 0.2);
-`
-const Heder = styled.h2`
-            color:#fff;
-`
-const LabelContainer = styled.label`
-            display:flex;
-            align-items:center;
-            flex-direction:column;
-            width:100%;
-            margin-bottom: 15px;
-            color:#fff;
-`
-const UserInput = styled.input`
-        width: 100%;
-        height: 40px;
-        margin-top:10px;
-        padding: 14px 0px 14px 5px;
-        font-size: 14px;
-        border: 1px solid rgba(33, 33, 33, 0.2);
-        border-radius: 4px;
-        outline: none;
-        transition: border-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
-        &:hover,
-        &:focus {
-            border-color: #ff6c00;
-        
-        margin-top:10px
-`
 export default Filter;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components'
+import {Container, Header,FormContainer, LabelContainer, UserInput, StyledBtn } from './Form.styled';
 
 class Form extends Component {
     state = {
@@ -72,75 +72,4 @@ class Form extends Component {
     }
 }
 
-const Container = styled.div`
-            max-width:450px;
-            margin-left:auto;
-            margin-right:auto;
-            margin-bottom:30px;
-            padding:15px;
-            background: #555555;
-            border-radius: 4px;
-            box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
-                0px 2px 1px rgba(0, 0, 0, 0.2);
-`
-const Header = styled.h1`
-            margin-top:0;
-            color:#fff;
-`
-const FormContainer = styled.form`
-            display:flex;
-            align-items:center;
-            flex-direction:column;
-            width:100%;
-            margin-left:auto;
-            margin-right:auto;
-             
-`
-const LabelContainer = styled.label`
-            display:flex;
-            align-items:center;
-            flex-direction:column;
-            width:100%;
-            margin-bottom: 15px;
-            color:#fff;
-`
-const UserInput = styled.input `
-        width: 100%;
-        height: 40px;
-        margin-top:10px;
-        padding: 14px 0px 14px 45px;
-        font-size: 18px;
-        border: 1px solid rgba(33, 33, 33, 0.2);
-        border-radius: 4px;
-        outline: none;
-        transition: border-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-
-        &:hover,
-        &:focus {
-            border-color: #ff6c00;
-        
-`
-
-const StyledBtn = styled.button`
-            min-width: 160px;
-            max-height: 40px;
-            border: 1px solid #ff6c00;
-            border-radius: 25px;
-            padding: 12px 20px;
-
-            font-weight: 600;
-            font-size: 12px;
-            line-height: 1.33;
-            letter-spacing: 0.09em;
-            color: #fff;
-            background-color: transparent;
-            transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-            text-transform: uppercase;
-            cursor: pointer;
-
-            &:hover, &:focus{
-                color:#000;
-                background-color:#ff6c00;
-            }
-`
 export default Form;
